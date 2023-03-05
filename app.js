dd398ba78f5410135ebd58cca9b98829d56b29ba
afifdgr <afifdgr3@gmail.com>
2023-03-05 21:37:59 +0700

    # Asynchronous Basics

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("starting");
+
+setTimeout(() => {
+  console.log("5 second timer");
+}, 5000);
+
+setTimeout(() => {
+  console.log("3 second timer");
+}, 3000);
+
+setTimeout(() => {
+  console.log("0 second timer");
+}, 0);
+
+setTimeout(() => {
+  console.log("1 second timer");
+}, 1000);
+
+console.log("stopping");
