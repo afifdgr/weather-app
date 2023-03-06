07bcc06dd45ca2968d7b6055e2e173bf08e1159b
afifdgr <afifdgr3@gmail.com>
2023-03-06 10:27:52 +0700

    # Customizing HTTP Requests

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const request = require("postman-request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=8c40e000413e99e5077a81829f481ddb&query=-7.399553382160208,109.67992040301874";
+  "http://api.weatherstack.com/current?access_key=8c40e000413e99e5077a81829f481ddb&query=-7.399553382160208,109.67992040301874&units=f";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  console.log("Temperature currently is " + response.body.current.temperature);
 });
