9e1ba559c7898aea0dd35dfa91ff7b770e227308
afifdgr <afifdgr3@gmail.com>
2023-03-06 22:06:06 +0700

    # Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,31 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello Express!");
+  res.send("<h1>Hello Express!<h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help Page");
+  res.send([
+    {
+      name: "Muhammad",
+    },
+    {
+      name: "Afiv",
+    },
+  ]);
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.send("<h1>About Page</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Your Weather ");
+  res.send([
+    {
+      forecast: "It is snowing",
+      location: "Banjarnegara",
+    },
+  ]);
 });
 
 app.listen(3000, () => {
