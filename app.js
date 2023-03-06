0d7944261c49e5167e63d115ae954d19c470c1e7
afifdgr <afifdgr3@gmail.com>
2023-03-06 13:36:41 +0700

    # An HTTP Request Challenge

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const request = require("postman-request");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=8c40e000413e99e5077a81829f481ddb&query=-7.399553382160208,109.67992040301874&units=f";
+// const url =
+//   "http://api.weatherstack.com/current?access_key=8c40e000413e99e5077a81829f481ddb&query=-7.399553382160208,109.67992040301874&units=f";
 
-request({ url: url, json: true }, (error, response) => {
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("unable to connect to weather service");
+//   } else if (response.body.error) {
+//     console.log("unable to find location");
+//   } else {
+//     console.log(
+//       "Temperature currently is " + response.body.current.temperature
+//     );
+//   }
+// });
+
+const geocode = "https://geocode.maps.co/reverse?lat=-7.399586&lon=109.679856";
+
+request({ url: geocode, json: true }, (error, response) => {
   if (error) {
-    console.log("unable to connect to weather service");
+    console.log("Unable to connect to location service");
   } else if (response.body.error) {
-    console.log("unable to find location");
+    console.log("unable find location");
   } else {
-    console.log(
-      "Temperature currently is " + response.body.current.temperature
-    );
+    const latitude = response.body.lat;
+    const longtitude = response.body.lon;
+    console.log(latitude, longtitude);
   }
 });
-
-// Geocoding
-// Address -> Lat/Long -> weather
