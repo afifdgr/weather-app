2a2d2512bc7b8027a19f58440482116bc9a2f581
afifdgr <afifdgr3@gmail.com>
2023-03-06 17:02:35 +0700

    # Callback Chaining

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const request = require("postman-request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Banjarnegara", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+const address = process.argv[2];
 
-forecast(-7.3995793101922835, 109.67985841779118, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+if (!address) {
+  console.log("Please provide an address");
+} else {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
