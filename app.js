d7b51c84554feb9742ff4a4c022ff20cd2da0f59
afifdgr <afifdgr3@gmail.com>
2023-03-06 11:09:08 +0700

    # Handling Errors

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ const url =
   "http://api.weatherstack.com/current?access_key=8c40e000413e99e5077a81829f481ddb&query=-7.399553382160208,109.67992040301874&units=f";
 
 request({ url: url, json: true }, (error, response) => {
-  console.log("Temperature currently is " + response.body.current.temperature);
+  if (error) {
+    console.log("unable to connect to weather service");
+  } else if (response.body.error) {
+    console.log("unable to find location");
+  } else {
+    console.log(
+      "Temperature currently is " + response.body.current.temperature
+    );
+  }
 });
+
+// Geocoding
+// Address -> Lat/Long -> weather
